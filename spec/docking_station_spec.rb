45c2288d735c81bbf5841af205f9992ee73fb5e2
mongolianprincess <ayc.yang@gmail.com>
2015-10-13 19:06:18 +0100

    Add 2nd test to Bike class and working? method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,5 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-  it "should respond to the release_bike method" do
-    expect(DockingStation.new).to respond_to :release_bike
-  end
-
   it { is_expected.to respond_to :release_bike }
-
 end
