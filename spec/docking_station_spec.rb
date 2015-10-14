6bb04006ccf785955e700c2cdc2dd8a050a93355
mongolianprincess <ayc.yang@gmail.com>
2015-10-14 11:28:09 +0100

    Add test to DockingStation to create bike when release_bike is called

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,10 @@ require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 end
+
+describe DockingStation do
+  it "creates a bike when release_bike is called" do
+    bike = subject.release_bike
+    expect(bike).to be_instance_of Bike
+end
+end
