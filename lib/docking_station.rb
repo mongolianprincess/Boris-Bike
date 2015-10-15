be8ce171081a98c472ef85afd7cfcd359c1a6bd5
mongolianprincess <ayc.yang@gmail.com>
2015-10-15 11:18:58 +0100

    Added private method, end of ch.15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,15 +9,24 @@ class DockingStation
   end
 
   def release_bike
-    raise 'shit there is no bike!' unless @bikes.length > 0
+    raise 'shit there is no bike!' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise 'docking station is full' if @bikes.length >= 20
+    raise 'docking station is full' if full?
     # raise error when bike is equal or more than 20
     @bikes << bike
     # if not, push bike into @bikes array
+  end
+
+  private
+  def full?
+    @bikes.length>=20
+  end
+
+  def empty?
+    @bikes.length> 0
   end
 
 end
